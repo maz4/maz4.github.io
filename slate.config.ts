c452c831d077e4a48e76c308198b2f89e3c18f6a
Marcin Zasadzki <zasadzkimarcin@gmail.com>
2026-01-22 15:09:29 +0900

    chore: update config

--- slate.config.ts
+++ slate.config.ts
@@ -5,23 +5,25 @@ import { defineConfig } from './src/helpers/config-helper';
 
 export default defineConfig({
   lang: 'en-US',
-  site: 'https://slate-blog-demo.vercel.app',
+  site: 'https://maz4.github.io',
   avatar: '/avatar.png',
   title: 'maz4 Blog',
   description: 'Thoughts, stories, experiments.',
   lastModified: true,
   readTime: true,
   footer: {
-    copyright: '© 2025 maz4',
+    copyright: '© 2026 maz4',
   },
   socialLinks: [
     {
       icon: 'github',
       link: 'https://github.com/maz4',
+      ariaLabel: 'maz4 github account',
     },
     {
       icon: 'link',
       link: 'https://www.linkedin.com/in/marcin-zasadzki-804b0184',
+      ariaLabel: 'maz4 linkedin profile',
     },
   ],
 });
